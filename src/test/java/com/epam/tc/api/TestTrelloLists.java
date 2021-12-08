d2e9d9d8a26f5f14007a465144bd9916634c9ae2
IuriiRadomskii <uraradomsky@gmail.com>
2021-12-08 10:32:04 +0300

    save

--- src/test/java/com/epam/tc/api/TestTrelloLists.java
+++ src/test/java/com/epam/tc/api/TestTrelloLists.java
@@ -56,19 +56,25 @@ public class TestTrelloLists extends BaseTest {
         Response createResponse = listSteps
             .createList("List", testBoard, RequestSpecifications.DEFAULT_SPEC, creds);
         TrelloList initList = ServiceObject.jsonListToPojo(createResponse);
+        initList.setName("newName");
 
-        Response modifyResponse = TrelloServiceObj.putTrelloListName("FOO", testBoard.getId(), initList.getId());
-        TrelloList modifiedList = TrelloServiceObj.getTrelloList(modifyResponse);
+        Response modifyResponse = listSteps.putListName(initList, RequestSpecifications.DEFAULT_SPEC, creds);
+        TrelloList modifiedList = ServiceObject.jsonListToPojo(modifyResponse);
 
-        modifyResponse.then().assertThat().spec(ResponseSpecifications.goodResponse);
-        assertThat("Checking put list name", modifiedList.getName(), Matchers.equalTo("FOO"));
+        modifyResponse
+            .then()
+            .assertThat()
+            .spec(ResponseSpecifications.goodResponse);
+
+        assertThat("Checking put list name", modifiedList.getName(), Matchers.equalTo("newName"));
         assertThat("Checking lists boardID", modifiedList.getIdBoard(), Matchers.equalTo(testBoard.getId()));
     }
 
     @Test
     public void checkListArchiving() {
-        Response createResponse = TrelloServiceObj.createTrelloList("List", testBoard.getId());
-        TrelloList initList = TrelloServiceObj.getTrelloList(createResponse);
+        Response createResponse = listSteps
+            .createList("List", testBoard, RequestSpecifications.DEFAULT_SPEC, creds);
+        TrelloList initList = ServiceObject.jsonListToPojo(createResponse);
 
         Response modifyResponse = TrelloServiceObj.putTrelloListName("FOO", testBoard.getId(), initList.getId());
         TrelloList modifiedList = TrelloServiceObj.getTrelloList(modifyResponse);
